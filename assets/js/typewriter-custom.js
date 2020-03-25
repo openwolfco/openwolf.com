f1d7a470e428c4d4e4974329da262a6f19e32200
openwolfco <support@openwolf.com>
2020-03-25 14:49:32 +0800

    update site

---
+++ assets/js/typewriter-custom.js
@@ -0,0 +1,8 @@
+/* ========= Typewriter effect =========== */
+/* Ref: https://safi.me.uk/typewriterjs/ */
+const instance = new Typewriter('#typewriter', {
+  strings: ['Blazing Fast', 'High Quality', 'Secure', 'User Friendly', 'Responsive'],
+  autoStart: true,
+  loop: true,
+});
+
